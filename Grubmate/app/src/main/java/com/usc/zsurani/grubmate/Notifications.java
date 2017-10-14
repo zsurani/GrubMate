8c8deb565be80b1025665ac47abe42533d17a681
Casey Klecan <caseyklecan@gmail.com>
2017-10-14 12:29:13 -0700

    updated notification classes, add group members UI, etc

--- Grubmate/app/src/main/java/com/usc/zsurani/grubmate/Notifications.java
+++ Grubmate/app/src/main/java/com/usc/zsurani/grubmate/Notifications.java
@@ -145,6 +145,6 @@ public class Notifications {
     /*
      * Sets or changes the end time set in the notification
      */
-    void setEndTime(Integer time){endTIme = time;}
+    void setEndTime(Integer time){endTime = time;}
 
 }
