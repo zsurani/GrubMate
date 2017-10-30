cff5e9d51df5d1e49be1c5c34aaacc02aefd397a
Zahra Surani <surani@usc.edu>
2017-10-29 17:26:21 -0700

    finished basic transaction

--- Grubmate/app/src/androidTest/java/com/usc/zsurani/grubmate/TransactionHistoryTest.java
+++ Grubmate/app/src/androidTest/java/com/usc/zsurani/grubmate/TransactionHistoryTest.java
@@ -11,10 +11,13 @@ import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
+import android.widget.ListView;
 
 import com.facebook.FacebookSdk;
 
+import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -25,10 +28,12 @@ import java.util.List;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.facebook.FacebookSdk.getApplicationContext;
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by Madison on 10/28/17.
@@ -39,7 +44,6 @@ public class TransactionHistoryTest {
     SQLiteDatabase db;
     Context c;
     UserRepo ur;
-    String user_name;
 
     @Before
     public void setup() {
@@ -52,30 +56,56 @@ public class TransactionHistoryTest {
         mActivityRule.getActivity()
                 .getSupportFragmentManager().beginTransaction();
 
-        UserRepo userRepo = new UserRepo(InstrumentationRegistry.getTargetContext());
+        UserRepo userRepo = new UserRepo(getApplicationContext());
         Profiles p = new Profiles();
-        p.setName("Madison Snyder");
-        p.setId("10204210117208549");
-        String uri =  "https://graph.facebook.com/10204210117208549/picture?height=2147483647&width=2147483647&migration_overrides=%7Boctober_2012%3Atrue%7D";
+        p.setName("Zahra Surani");
+        p.setId("1353924581401606");
+        String uri =  "https://graph.facebook.com/1353924581401606/picture?height=2147483647&width=2147483647&migration_overrides=%7Boctober_2012%3Atrue%7D";
         p.setUri(Uri.parse(uri));
 
         userRepo.insertProfile(p);
-        User user = new User("Madison Snyder", "10204210117208549");
+        User user = new User("Zahra Surani", "1353924581401606");
         userRepo.insert(user);
 
-        GroupRepo gr = new GroupRepo(getApplicationContext());
-        List<String> m = new ArrayList<String>();
-        m.add("1");
-        gr.insert("GROUP", m);
+        Post post = new Post("description", "1353924581401606", "food", null, "1", "categories", "tags",
+                "10:00 pm", "11:00 pm", "home", "true", "", "", "homemade");
+
+        PostRepo postRepo = new PostRepo(getApplicationContext());
+        postRepo.insert(post);
+
+        Transaction transaction = new Transaction(1,
+                1, "location", 1);
+        TransactionRepo tr = new TransactionRepo(getApplicationContext());
+        transaction.setStatus("OPEN");
+        tr.insert(transaction);
     }
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void testGroup() {
+    public void testPostAdapter() {
         onView(withId(R.id.drawer_layout)).perform(actionOpenDrawer());
         onView(withText("My Transactions")).perform(click());
+
+        final int[] counts = new int[1];
+        onView(withId(R.id.list_transaction_hist)).check(matches(new TypeSafeMatcher<View>() {
+            @Override
+            public boolean matchesSafely(View view) {
+                ListView listView = (ListView) view;
+
+                counts[0] = listView.getCount();
+
+                return true;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+
+            }
+        }));
+
+        assertEquals(counts[0], 1);
     }
 
     private static ViewAction actionOpenDrawer() {
